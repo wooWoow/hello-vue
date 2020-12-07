f683c46d33c767cdba845239ba0f7339aaa1a301
wooWoow <wuhaoyang9307@163.com>
2020-12-07 23:09:39 +0800

    执行命令完善

--- src/components/CommondInfo.vue
+++ src/components/CommondInfo.vue
@@ -2,9 +2,19 @@
   <div class="main-box">
     <h2 class="top-title-box mt-20">{{ $t('run_commond') }}</h2>
     <a-input class="commond-input" placeholder="input commond" v-model="commandStr" />
-    <a-button type="primary" html-type="submit" @click="commindSearch">
+    <a-button type="primary" html-type="submit" @click="commindSearch(false)">
       {{ $t('btn_query') }}
     </a-button>
+
+    <div class="commondItemBox">
+      <template v-for="(item, key) in commandArr" >
+        <span class="commondItem" v-bind:key="key">
+          {{item}}
+          <i class="delCommondItem cursor-pointer" @click="delCommondItem(key)">x</i>
+        </span>
+        <span v-if="commandArr.length !== (key + 1)" v-bind:key="key">  &&  </span>
+      </template>
+    </div>
 
     <div class="monaco-box">
       <div id='container' ref="container"></div>
@@ -21,6 +31,7 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
 export default class GoldInfo extends Vue {
   // 命令
   commandStr = '';
+  commandArr: any = [];
 
   monacoEditor: any = null;
 
@@ -48,18 +59,38 @@ export default class GoldInfo extends Vue {
     this.monacoEditor = monaco.editor.create(edit, this.monacoConfig);
   };
 
-  commindSearch () {
+  commindSearch (skip: boolean) {
     const that = this;
-    Request.post('v1/info/sysInfo', {
-      params: {
-        commond: that.commandStr
+    if (this.commandStr || skip) {
+      if (!skip) {
+        this.commandArr.push(this.commandStr);
+        this.commandStr = '';
       }
-    }).then(res => {
-      if (res.data.code === 200) {
-        that.monacoEditor.setValue(res.data.data.info);
-      }
-    });
+
+      Request.post('v1/info/sysInfo', {
+        params: {
+          commond: this.commandArr.join('&&')
+        }
+      }).then(res => {
+        if (res.data.code === 200) {
+          that.monacoEditor.setValue(res.data.data.info);
+        }
+      });
+    } else {
+      that.$message.info('查询参数不能为空！');
+    }
   };
+
+  delCommondItem(index) {
+    console.log(index);
+    if (index === 0) {
+      this.commandArr = [];
+      this.monacoEditor.setValue('');
+    } else {
+      this.commandArr = this.commandArr.slice(0, index);
+      this.commindSearch(true);
+    }
+  }
 }
 </script>
 
@@ -90,5 +121,29 @@ export default class GoldInfo extends Vue {
 .top-select-box {
   min-width: 300px;
   margin: 20px 60px;
+}
+.commondItem {
+  border: 1px solid #999;
+  border-radius: 3px;
+  padding: 0 8px;
+  margin-right: 5px;
+  line-height: 20px;
+}
+.commondItemBox {
+  margin: 5px 20px;
+}
+.delCommondItem {
+  display: inline-block;
+  vertical-align: middle;
+  width: 12px;
+  height: 12px;
+  line-height: 10px;
+  border-radius: 12px;
+  background-color: #bbb;
+  margin: 0px;
+  margin-left: 5px;
+  padding: 0px;
+  font-size: 10px;
+  text-align: center;
 }
 </style>
